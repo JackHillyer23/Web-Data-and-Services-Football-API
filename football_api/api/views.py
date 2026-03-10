b7fd5c8986f5cc4e9d93a3beff3d7e53d9bf3185
Jack <sc23jh@leeds.ac.uk>
2026-03-10 15:52:11 +0000

    implemented CRUD viewsets for all models

--- football_api/api/views.py
+++ football_api/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Team, Player, Match, PlayerMatchStats
+from .serializers import TeamSerializer, PlayerSerializer, MatchSerializer, PlayerMatchStatsSerializer
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+
+class PlayerMatchStatsViewSet(viewsets.ModelViewSet):
+    queryset = PlayerMatchStats.objects.all()
+    serializer_class = PlayerMatchStatsSerializer
